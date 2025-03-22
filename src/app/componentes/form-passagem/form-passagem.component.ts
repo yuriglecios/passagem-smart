d6522c25f2c22c1e6b71219ac3e29a95ff281e21
yuri glecios <glleciosyuri@gmail.com>
2025-03-22 00:11:01 -0300

    finalizando a primeira etapa do formulário

--- src/app/componentes/form-passagem/form-passagem.component.ts
+++ src/app/componentes/form-passagem/form-passagem.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AutoComplete } from 'primeng/autocomplete';
 import { aeroportosLista, Aeroporto } from '../../model/aeroportos-lista';
 
@@ -8,7 +8,8 @@ import { aeroportosLista, Aeroporto } from '../../model/aeroportos-lista';
   selector: 'app-form-passagem',
   imports: [
     FormsModule,
-    AutoComplete
+    AutoComplete,
+    ReactiveFormsModule
   ],
   templateUrl: './form-passagem.component.html',
   styleUrl: './form-passagem.component.css'
@@ -16,15 +17,22 @@ import { aeroportosLista, Aeroporto } from '../../model/aeroportos-lista';
 export class FormPassagemComponent {
   passagemForm: FormGroup;
   aeroportos: Aeroporto[] = aeroportosLista;
-  filteredAirports: Aeroporto[] = [];
+  filtroAeroportosOrigem: Aeroporto[] = [];
+  filtroAeroportosDestino: Aeroporto[] = [];
   origemValue: any;
   destinoValue: any;
 
-  search(event: {query: string}){
-    this.filteredAirports = this.aeroportos.filter(aeroporto =>
-      aeroporto.name.toLowerCase().includes(event.query.toLowerCase()) ||
-      aeroporto.iataCode.toLowerCase().includes(event.query.toLowerCase())
-    )
+  searchOrigem(event: { query: string }) {  
+    this.filtroAeroportosOrigem = this.aeroportos.filter(aeroporto =>
+        aeroporto.name.toLowerCase().includes(event.query.toLowerCase()) ||
+        aeroporto.iataCode.toLowerCase().includes(event.query.toLowerCase())
+    );
+  }
+  searchDestino(event: { query: string }) {
+    this.filtroAeroportosDestino = this.aeroportos.filter(aeroporto =>
+        aeroporto.name.toLowerCase().includes(event.query.toLowerCase()) ||
+        aeroporto.iataCode.toLowerCase().includes(event.query.toLowerCase())
+    );
   }
 
   constructor(private fb: FormBuilder) {
@@ -42,5 +50,14 @@ export class FormPassagemComponent {
       taxaPassageiro: ['']
     });
   }
+
+  onSubmit() {
+    if (this.passagemForm.valid) {
+        console.log('Valores do formulário:', this.passagemForm.value);
+        // Aqui você faria a lógica para buscar as passagens com os dados do formulário
+    } else {
+        // Lógica para exibir erros de validação
+    }
+}
 
 }
